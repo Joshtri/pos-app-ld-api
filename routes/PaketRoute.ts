7f8559edc0c937ab2a10d8979c0e058fab2c2905
Joshtri <stuffofyos1516@gmail.com>
2024-12-15 18:04:43 +0800

    updating paket

--- routes/PaketRoute.ts
+++ routes/PaketRoute.ts
@@ -1,17 +1,33 @@
-import express from 'express'
-import type {Request, Response} from 'express';
-import { PaketController } from '../controllers/PaketController';
+import express from "express";
+import type { Request, Response } from "express";
+import { PaketController } from "../controllers/PaketController";
 
 const paketRoute = express.Router();
 const paketController = new PaketController();
 
+// Mendapatkan semua paket
+paketRoute.get("/paket", (req: Request, res: Response) => {
+  paketController.getAllPaket(req, res);
+});
 
-paketRoute.get('/paket',(req:Request, res:Response)=>{
-    paketController.getAllPaket(req,res);
-})
+// Membuat paket baru
+paketRoute.post("/paket", (req: Request, res: Response) => {
+  paketController.createPaket(req, res);
+});
 
-paketRoute.post('/paket',(req:Request, res:Response)=>{
-    paketController.createPaket(req,res);
+// Mendapatkan paket berdasarkan ID
+paketRoute.get("/paket/:id", (req: Request, res: Response) => {
+  paketController.getPaketById(req, res);
+});
+
+// Memperbarui paket berdasarkan ID
+paketRoute.patch("/paket/:id", (req: Request, res: Response) => {
+  paketController.updatePaket(req, res);
+});
+
+// Menghapus paket berdasarkan ID
+paketRoute.delete("/paket/:id", (req: Request, res: Response) => {
+  paketController.deletePaket(req, res);
 });
 
 export default paketRoute;
