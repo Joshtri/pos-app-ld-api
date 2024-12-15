109d37cf756173ea68d398d1b5e68c4f94313158
Joshtri <stuffofyos1516@gmail.com>
2024-12-15 17:34:14 +0800

    update endpoint and add patch method

--- routes/ParfumRoute.ts
+++ routes/ParfumRoute.ts
@@ -1,17 +1,33 @@
-import express from 'express'
-import type {Request, Response} from 'express';
-import { ParfumController } from '../controllers/ParfumController.ts';
+import express from "express";
+import type { Request, Response } from "express";
+import { ParfumController } from "../controllers/ParfumController";
 
 const parfumRoute = express.Router();
 const parfumController = new ParfumController();
 
+// Mendapatkan semua parfum
+parfumRoute.get("/parfum", (req: Request, res: Response) => {
+  parfumController.getAllParfum(req, res);
+});
 
-parfumRoute.get('/parfum',(req: Request, res: Response)=>{
-    parfumController.getAllParfum(req,res);
-})
+// Membuat parfum baru
+parfumRoute.post("/parfum", (req: Request, res: Response) => {
+  parfumController.createParfum(req, res);
+});
 
-parfumRoute.post('/parfum',(req: Request, res: Response)=>{
-    parfumController.createParfum(req,res);
-})
+// Memperbarui parfum berdasarkan ID (PATCH untuk update parsial)
+parfumRoute.patch("/parfum/:id", (req: Request, res: Response) => {
+  parfumController.updateParfum(req, res);
+});
+
+// Menghapus parfum berdasarkan ID
+parfumRoute.delete("/parfum/:id", (req: Request, res: Response) => {
+  parfumController.deleteParfum(req, res);
+});
+
+// Mendapatkan parfum berdasarkan ID
+parfumRoute.get("/parfum/:id", (req: Request, res: Response) => {
+  parfumController.getParfumById(req, res);
+});
 
 export default parfumRoute;
